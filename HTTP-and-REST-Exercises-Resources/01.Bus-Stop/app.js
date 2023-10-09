3e71eb4628ff86cf8bb1c25a528e6d462b828e59
Desislava Zafirova <114089205+DesZafirova@users.noreply.github.com>
2023-10-09 18:36:05 +0300

    Add files via upload

---
+++ HTTP-and-REST-Exercises-Resources/01.Bus-Stop/app.js
@@ -0,0 +1,19 @@
+function getInfo() {
+    const busStopId = document.querySelector("#stopId").value;
+    const list = document.querySelector("ul");
+    list.innerHTML = "";
+    fetch(`http://localhost:3030/jsonstore/bus/businfo/${busStopId}`)
+        .then(res => res.json())
+        .then(busStop => {
+            document.querySelector("#stopName").textContent = busStop.name;
+            Object.entries(busStop.buses).map((busNumber, timeInMinutes) => {
+                const item = document.createElement('li');
+                item.textContent = `Bus ${busNumber} arrives in ${timeInMinutes} minutes`;
+
+                list.appendChild(item);
+            });
+        }).catch(() => {
+        document.querySelector("#stopName").textContent = "Error";
+
+    })
+}
